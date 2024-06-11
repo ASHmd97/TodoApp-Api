f369c802e5047c9b877e8853b35f0744b384cadd
Ahmed Safwat <ahmed.hmd1996@gmail.com>
2024-06-11 08:48:35 +0300

    add cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ require("./db");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use(morgan("dev"));
 // ------------------------------------------------------------------------------
 
